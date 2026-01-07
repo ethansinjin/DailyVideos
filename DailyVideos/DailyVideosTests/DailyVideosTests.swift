b8a2409d370effbb5b3e9c18a0e5d8f7996f3f8a
Ethan Gill <ethansinjin@gmail.com>
2026-01-07 07:37:41 -0500

    Create Tests and UI Tests targets

---
+++ DailyVideos/DailyVideosTests/DailyVideosTests.swift
@@ -0,0 +1,16 @@
+//
+//  DailyVideosTests.swift
+//  DailyVideosTests
+//
+//  Created by Ethan Gill on 1/7/26.
+//
+
+import Testing
+
+struct DailyVideosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
